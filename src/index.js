ac531a8ff0d221801fbd9615ebf5ddcf3d69df98
Lucas Vasconcelos <lucas.vasconcelos915@gmail.com>
2021-11-02 14:56:11 -0300

    adicionado a lista de statements e delete de uma account

--- src/index.js
+++ src/index.js
@@ -64,6 +64,20 @@ app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
     return response.json(customer.statement);
 });
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) =>
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
+
 app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
     const { description, amount } = request.body;
 
@@ -102,6 +116,41 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
     return response.status(201).send();
 
 });
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+   const { name } = request.body;
+   const { customer } = request;
+
+   customer.name = name;
+
+  return response.status(201).json(customer);
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+   const { customer } = request;
+
+   console.log(customers);
+
+   customers.splice(customer, 1);
+
+   console.log(customers);
+
+   return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+   const { customer } = request;
+
+   const balance = getBalance(customer.statement);
+
+   return response.json(balance);
+})
 
 app.listen(3333);
 
